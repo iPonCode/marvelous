e9240052b158b2fb2367bb3204b3ceecb15fabfc
Simon <simon@aparicio.org>
2020-10-03 23:30:15 +0200

    [Config]: created new project 	- group folder hierarchy 	- .gitignore added 	-  AppIcon added

---
+++ marvelous/Sources/Modules/ViewController.swift
@@ -0,0 +1,19 @@
+//  ViewController.swift
+//  marvelous
+//
+//  Created by Simón Aparicio on 03/10/2020.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
